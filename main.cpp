5976e0654ba6c46de3dd15cc7c04ab820d717302
HimalaGunathilaka <himala.22@cse.mrt.ac.lk>
2026-03-24 07:52:22 +0530

    feat: Throw runtime errors when invalid element is given

--- main.cpp
+++ main.cpp
@@ -7,21 +7,40 @@
 
 using namespace std;
 
-vector<string> types = {"Rose", "Lavender", "Lotus", "Tulip", "Orchid"};
+vector<string> types = {"rose", "lavender", "lotus", "tulip", "orchid"};
+
+
 order_manager order_mgr(types);
 
 int main()
 {
     rapidcsv::Document doc("data/orders.csv");
+    // rapidcsv::Document execution_rep("data/exection_rep.csv");
 
     int num_of_rows = doc.GetRowCount();
 
     for (size_t i = 0; i < num_of_rows; i++)
     {
         vector<string> row = doc.GetRow<string>(i);
-        order_mgr.insertOrder(row);
+        try{
+            order_mgr.insertOrder(row);
+        }catch(const exception& e){
+            cerr<<"Error detected: "<<e.what()<<endl;
+        }
     }
 
     order_mgr.print_stats();
+
+    // rapidcsv::Document doc("data/execution_rep.csv");
+    
+    // for(size_t i=0; i< doc.GetRowCount();i++){
+    //     vector<string> row = doc.GetRow<string>(i);
+    //     cout<<endl;
+    //     for(string ele:row){
+    //         cout<<ele<<" ";
+    //     }
+
+    //     cout<<i<<endl;
+    // }
     return 0;
 }
