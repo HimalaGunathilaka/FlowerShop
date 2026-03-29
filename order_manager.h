b855ba0e1477b372778a31b33eece34defc82741
Himala Gunathilaka <himala.22@cse.mrt.ac.lk>
2026-03-29 18:04:18 +0530

    feat: Throw runtime errors when invalid element is given (#7)

--- order_manager.h
+++ order_manager.h
@@ -20,14 +20,16 @@
 #include <utility>
 #include <set>
 #include <list>
+#include <stdexcept>
 
 using namespace std;
+
 
 struct Order{
     double price;
     int qty;
     array<char,8> id;
-} temp_sell, temp_buy;
+} temp_order;
 
 struct CompareSellPrice{
     bool operator()(const Order& a, const Order& b) const{
@@ -57,6 +59,7 @@ private:
      *
      */
     unordered_map<string, OrderBook> books;    
+    unordered_set<string> valid_types;
 
 public:
     order_manager(vector<string> types);
@@ -72,6 +75,8 @@ order_manager::order_manager(vector<string> types)
     {
         string type = types[i];
         transform(type.begin(), type.end(), type.begin(), ::tolower);
+        // Insert to set of valid types
+        valid_types.insert(type);
         books.insert({type, OrderBook()});
     }
 }
@@ -88,31 +93,49 @@ order_manager::~order_manager()
  */
 void order_manager::insertOrder(vector<string> new_order)
 {
+    if(new_order.size() < 5){
+        throw runtime_error("All elements must not be empty!");
+    }
     string type = new_order[1];
     transform(type.begin(), type.end(), type.begin(), ::tolower);
-    // cout<<type<<endl;
-    // Buy
-    if (new_order[2] == "1")
+
+    if(valid_types.find(type) == valid_types.end()){
+        throw runtime_error("Invalid type!");
+    }
+
+    for (int j = 0; j < 7; j++)
     {
-        for (int j = 0; j < 7; j++)
-        {
-            temp_buy.id[j] = new_order[0][j];
-        }
-        temp_buy.price = stod(new_order[3]);
-        temp_buy.qty = stoi(new_order[4]);
-        books.at(type).asc_buy_side[temp_buy.price].push_back(temp_buy);
+        temp_order.id[j] = new_order[0][j];
+    }
+
+    temp_order.qty = stod(new_order[3]);
+    // Throw error when invalid quantity
+    if(temp_order.qty % 10 != 0){
+        // cout<<temp_order.qty<<endl;
+        throw runtime_error("Invalid quantity! Must be multiple of 10!");
+    }else if(temp_order.qty < 10 || temp_order.qty > 1000){
+        throw runtime_error("Quantity is not within valid range!");
+    }
+
+
+    temp_order.price = stoi(new_order[4]);
+    // Throw error when invalid price
+    if(temp_order.price < 0){
+        throw std::runtime_error("Invalid price! Must be positive.");
+    }
+
+    // Buy
+    if(new_order[2] == "1"){
+        books.at(type).asc_buy_side[temp_order.price].push_back(temp_order);
     }
     // Sell
-    else if (new_order[2] == "2")
-    {
-        for (int j = 0; j < 7; j++)
-        {
-            temp_sell.id[j] = new_order[0][j];
-        }
-        temp_sell.price = stod(new_order[3]);
-        temp_sell.qty = stoi(new_order[4]);
-        books.at(type).desc_sell_side[temp_sell.price].push_back(temp_sell);
+    else if(new_order[2] == "2"){
+        books.at(type).desc_sell_side[temp_order.price].push_back(temp_order);
+    }else{
+        throw runtime_error("Invalid side!");
     }
+
+    
 }
 
 void order_manager::print_stats(){
